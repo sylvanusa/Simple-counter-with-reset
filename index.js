0c912bfc619070b6e4730ff613272439bf41f906
Sylvanusa <s.carpenter83@gmail.com>
2022-10-02 14:49:19 -0400

    Simple traffic counter

---
+++ index.js
@@ -0,0 +1,20 @@
+let saveEl = document.getElementById("save-el");
+let countEl = document.getElementById("count-el");
+let count = 0;
+
+function increment() {
+  count += 1;
+  countEl.textContent = count;
+}
+
+function save() {
+  let countStr = count + " - ";
+  saveEl.textContent += countStr;
+  countEl.textContent = 0;
+  count = 0;
+}
+
+function reset() { 
+if(confirm("Are you sure you want to reset entries?")){
+  saveEl.textContent = "Previous entries:";}
+}
